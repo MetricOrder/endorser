12f861775d01c10cec2815c0cadba9cfb14c6389
MetricOrder <106072188+MetricOrder@users.noreply.github.com>
2023-12-07 14:37:00 +1100

    push function imported, const for input element and button element added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {databaseURL: "https://shout-out-9d6fa-default-rtdb.asia-southeast1.firebasedatabase.app/"}
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-
 const shoutoutsInDB = ref(database, "shoutouts")
+const shoutoutInputEl = document.querySelector("#main-input-field")
+const inputBtnEl = document.querySelector("#main-input-btn")
