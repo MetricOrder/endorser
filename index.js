a16f2cd4c5ba6e4693bc2da629101a3a52701c12
MetricOrder <106072188+MetricOrder@users.noreply.github.com>
2023-12-08 13:58:04 +1100

    event listener, clear input, and onvalue functions added
    more funtionality required for onvalue

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {databaseURL: "https://shout-out-9d6fa-default-rtdb.asia-southeast1.firebasedatabase.app/"}
 
@@ -8,3 +8,24 @@ const database = getDatabase(app)
 const shoutoutsInDB = ref(database, "shoutouts")
 const shoutoutInputEl = document.querySelector("#main-input-field")
 const inputBtnEl = document.querySelector("#main-input-btn")
+const shoutoutsDisplay = document.querySelector("#shoutouts-list")
+
+inputBtnEl.addEventListener ("click", function(){
+    let userInputValue = shoutoutInputEl.value
+
+    push(shoutoutsInDB, userInputValue) 
+    
+    clearInputField()
+})
+
+onValue(shoutoutsInDB, function(snapshot){
+    let shoutout = Object.value(snapshot.val())
+    shoutoutsDisplay.innerHTML = shoutout
+
+})
+
+
+
+function clearInputField(){
+    shoutoutInputEl.value = ""
+}
